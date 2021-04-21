bfed0ad25d8fa1cae5976ad372d409a5c2a7baa8
Adamchuk Kostyantyn <adamchukkostyantyn@hotmail.com>
2021-04-21 22:33:07 +0200

    end of section 8

--- API/Data/Migrations/20210420204745_ExtendedUserEntityPart2.Designer.cs
+++ API/Data/Migrations/20210420204745_ExtendedUserEntityPart2.Designer.cs
@@ -4,14 +4,16 @@ using API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace API.Data.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20210420204745_ExtendedUserEntityPart2")]
+    partial class ExtendedUserEntityPart2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ API/Data/Migrations/20210420204745_ExtendedUserEntityPart2.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Data.Migrations
+{
+    public partial class ExtendedUserEntityPart2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Url",
+                table: "Photos",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "Url",
+                table: "Photos",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
