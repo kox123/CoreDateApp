d6bd47e972bc308d5abb9aa8d6463beaed31a247
Adamchuk Kostyantyn <adamchukkostyantyn@hotmail.com>
2021-05-17 17:16:45 +0200

    end of section 13

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -6,6 +6,7 @@ using API.Data;
 using API.DTO;
 using API.Entities;
 using API.Extensions;
+using API.Helpers;
 using API.Interfaces;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
@@ -30,9 +31,18 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
+        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers([FromQuery] UserParams userParams)
         {
-            var users = await _userRepository.GetMembersAsync();
+            var user = await _userRepository.GetUserByUsernameAsync(User.GetUsername());
+            userParams.CurrentUsername = User.GetUsername();
+            if (string.IsNullOrWhiteSpace(userParams.Gender))
+            {
+                userParams.Gender = user.Gender == "male" ? "female" : "male";
+
+            }
+            var users = await _userRepository.GetMembersAsync(userParams);
+
+            Response.AddPaginationHeader(users.CurrentPage, users.PageSize, users.TotalCount, users.TotalPages);
 
             return Ok(users);
         }
@@ -142,7 +152,7 @@ namespace API.Controllers
             {
                 return Ok();
             }
-            
+
             return BadRequest("Failed to delete a photo");
         }
     }
--- client/src/app/_models/user.ts
+++ client/src/app/_models/user.ts
@@ -2,6 +2,8 @@ export interface User {
     username: string;
     token: string;
     photoUrl: string;
+    knownAs: string;
+    gender: string;
 } 
 
 // let data: number | string = 42;
