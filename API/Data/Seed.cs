9af9f05137d93d7b4b310c9477d70a3bf78f235e
Adamchuk Kostyantyn <adamchukkostyantyn@hotmail.com>
2021-06-03 23:45:21 +0200

    End of section 16

--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -1,36 +1,53 @@
 using System.Collections.Generic;
 using System.IO;
-using System.Security.Cryptography;
-using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 using API.Entities;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data
 {
     public class Seed
     {
-        public static async Task SeedUsers(DataContext context)
+        public static async Task SeedUsers(UserManager<AppUser> userManager, RoleManager<AppRole> roleManager)
         {
-            if (await context.Users.AnyAsync())
+            if (await userManager.Users.AnyAsync())
             {
                 return;
             }
 
             var userData = await File.ReadAllTextAsync("Data/UserSeedData.json");
             var users = JsonSerializer.Deserialize<List<AppUser>>(userData);
-            foreach (var user in users)
+            if (users == null)
             {
-                using var hmac = new HMACSHA512();
-                user.UserName = user.UserName.ToLower();
-                user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes("admin"));
-                user.PasswordSalt = hmac.Key;
-
-                context.Users.Add(user);
+                return;
             }
 
-            await context.SaveChangesAsync();
+            var roles = new List<AppRole>
+            {
+                new AppRole {Name = "Admin"},
+                new AppRole {Name = "Moderator"},
+                new AppRole {Name = "Member"}
+            };
+
+            foreach (var role in roles)
+            {
+                await roleManager.CreateAsync(role);
+            }
+            foreach (var user in users)
+            {
+                user.UserName = user.UserName.ToLower();
+                await userManager.CreateAsync(user, "admin");
+                await userManager.AddToRoleAsync(user, "Member");
+            }
+
+            var admin = new AppUser
+            {
+                UserName = "admin"
+            };
+            await userManager.CreateAsync(admin, "admin");
+            await userManager.AddToRolesAsync(admin, new[] { "Admin", "Moderator" });
         }
     }
 }
